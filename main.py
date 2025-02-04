180f11f083622f1d68bcacdd8c325f072625d1dc
Nicol Forero <165518210+Nifogu27@users.noreply.github.com>
2025-02-04 00:58:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def video():
 def carta():
     if request.method == 'POST':
         fecha = request.form.get('fecha')
-        if fecha == "07/05/24" or "07/05/2024:
+        if fecha == "07/05/24":
             return send_file('static/assets/carta.pdf', as_attachment=True)
         else:
             return "Respuesta incorrecta. Intenta de nuevo."
